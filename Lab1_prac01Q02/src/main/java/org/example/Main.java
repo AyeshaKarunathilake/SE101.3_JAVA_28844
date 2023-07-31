02d9e1068bf4bb4fd2977ee94910db7c4a918edd
AyeshaKarunathilake <141011698+AyeshaKarunathilake@users.noreply.github.com>
2023-07-31 06:03:57 -0700

    Add files via upload

---
+++ Lab1_prac01Q02/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ayesha Karunathilake");
+        System.out.println("Management Information Systems(Special)");
+    }
+}
